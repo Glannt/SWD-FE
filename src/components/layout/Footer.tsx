680bbfb78ec65aa81b86012c1cd8f018265448bd
Dan-G5 <dangtran2652004@>
2025-06-15 19:26:44 +0700

    feat: update homepage UI and structure

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => (
+  <footer className="bg-white mt-12">
+    <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+      <p className="text-center text-base text-gray-500">
+        &copy; 2024 FPTCareer. All rights reserved.
+      </p>
+    </div>
+  </footer>
+);
+
+export default Footer;
